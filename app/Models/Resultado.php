136e8bfb50b64cb51524f72c6867db0744602551
andres174 <zambrasda17@gmail.com>
2023-03-23 18:27:06 -0500

    model resultado

--- app/Models/Resultado.php
+++ app/Models/Resultado.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Resultado extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'puntos',
+        'goles_favor',
+        'goles_contra',
+        'id_equipo_disciplina',
+        'id_partido',
+        'estado',
+    ];
 }

2307ea9a0ea990cf5cea48fdb405b6796372c719
Luis Macias <luismaciasantonio2000@gmail.com>
2023-03-24 09:25:18 -0500

    apis jugador

--- app/Http/Controllers/JugadorEquipoController.php
+++ app/Http/Controllers/JugadorEquipoController.php
@@ -26,6 +26,7 @@ class JugadorEquipoController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+    
     public function store(Request $request)
     {
         //
